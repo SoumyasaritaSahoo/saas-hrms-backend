d364d64098bb7eb35a893ee0a49432c9ef624f72
ssahoo-techrayslabs <ssahoo@techrayslabs.com>
2026-07-21 12:01:29 +0530

    setup nest js projcet

---
+++ src/common/constants/company-meta-keys.constant.ts
@@ -0,0 +1,7 @@
+export const CompanyMetaKeys = {
+  FINANCIAL_YEAR_START_MONTH: 'financial_year_start_month',
+  FINANCIAL_YEAR_END_MONTH: 'financial_year_end_month',
+  WORKING_WEEK_DAYS: 'working_week_days',
+  COMPANY_SIZE: 'company_size',
+  LEAVE_POLICY_DETAILS: 'leave_policy_details',
+} as const;
